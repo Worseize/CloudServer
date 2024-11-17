124e711d3d154eda7d8b4fcbf19b53811a508cbb
Worseize <worseize@gmail.com>
2024-11-17 17:44:27 +0300

    Git upload

--- main.js
+++ main.js
@@ -1,39 +1,17 @@
-let width = 100;
-let height = 100;
-let Others = [];
-let thisPlayer;
-
-class Player{
-  constructor(posX, posY, power) {
-    this.posX = posX;
-    this.posY = posY;
-    this.power = power;
-  }
-  
-}
-class Other{
-  constructor(posX, posY, power) {
-    this.posX = posX;
-    this.posY = posY;
-    this.power = power;
-  }
-  
-}
 function setup() {
   createCanvas(width, height);
-  thisPlayer = new Player(width/2, height/2, width/20);
-  const oth1 = new Other(width/4, height/4, width/20);
-  const oth2 = new Other(width/8, height/8, width/20);
-  Others.push(oth1);
-  Others.push(oth2);
+  background(255);
+  for(let i = 0; i < Buttons.length; i++){
+    Buttons[i].show() 
+  }
 }
 
 function draw() {
-  background(255);
-  fill(0, 200, 0);
-  noStroke;
-  for(let i = 0; i < Others.length; i++){
-    ellipse(Others[i].posX, Others[i].posY, Others[i].power);
+  if(page == 1){
+
+  }else if(page == 2){
+
+  }else if(page == 3){
+
   }
-  ellipse(thisPlayer.posX, thisPlayer.posY, thisPlayer.power);
 }
